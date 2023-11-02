29d4c44695d4da8368361b94797655e27740f66b
Levi02-4 <139589750+Levi02-4@users.noreply.github.com>
2023-11-01 20:59:55 -0400

    connection.js file

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'event_management',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+
+  console.log('Connected to MySQL database!');
+});
+
+module.exports = connection;
