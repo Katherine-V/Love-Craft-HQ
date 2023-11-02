261f1174dbba35a75612e1923bd5023f7d5f5b7d
Levi02-4 <139589750+Levi02-4@users.noreply.github.com>
2023-11-01 21:18:50 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,16 @@
-const mysql = require('mysql');
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'event_management',
-});
+let sequelizeInstance;
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-    return;
-  }
+if (process.env.JAWSDB_URL) {
+  sequelizeInstance = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelizeInstance = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
-  console.log('Connected to MySQL database!');
-});
-
-module.exports = connection;
+module.exports = sequelizeInstance;
