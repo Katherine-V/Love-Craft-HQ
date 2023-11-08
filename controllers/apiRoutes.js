117321fcadc6f83aefa9daeba767ea96ea970025
Blackberry <david2631@gmail.com>
2023-11-08 13:21:25 -0500

    adding user auth (in dev still) and this version successfully deploys on heroku

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -16,14 +16,21 @@ router.post('/login/event-manager', async (req, res) => {
       return;
     }
 
-    const validPassword = eventData.checkPassword(req.body.password);
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect username or password, please try again' });
-      return;
-    }
+    //const validPassword = eventData.checkPassword(req.body.password);
+    //if (!validPassword) {
+    //  res.status(400).json({ message: 'Incorrect username or password, please try again' });
+    //  return;
+    //}
+
+    req.session.save(() => {
+      req.session.user_id = eventData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json({ user: eventData, message: 'You are now logged in!' });
+    });
 
     // Add code here to handle session/cookie if you're using them
-    res.json({ user: eventData, message: 'You are now logged in!' });
+    // res.json({ user: eventData, message: 'You are now logged in!' });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -38,14 +45,21 @@ router.post('/login/vendor', async (req, res) => {
       return;
     }
 
-    const validPassword = vendorData.checkPassword(req.body.password);
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect username or password, please try again' });
-      return;
-    }
+    // const validPassword = vendorData.checkPassword(req.body.password);
+    // if (!validPassword) {
+    //   res.status(400).json({ message: 'Incorrect username or password, please try again' });
+    //   return;
+    // }
+
+    req.session.save(() => {
+      req.session.user_id = vendorData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json({ user: vendorData, message: 'You are now logged in!' });
+    });
 
     // Add code here to handle session/cookie if you're using them
-    res.json({ user: vendorData, message: 'You are now logged in!' });
+    //res.json({ user: vendorData, message: 'You are now logged in!' });
   } catch (err) {
     res.status(500).json(err);
   }
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,13 +1,24 @@
-document.getElementById('loginForm').addEventListener('submit', function (e) {
+document.getElementById('loginForm').addEventListener('submit', async function (e) {
     e.preventDefault();
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const role = document.getElementById('role').value;
 
-    if (role === 'eventManagement') {
-        window.location.href = '/event-management';
-    } else if (role === 'vendor') {
-        window.location.href = '/vendor';
+    const response = await fetch(`/api/login/${role}`, {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            password 
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (response.ok){
+        const route = role === "vendor"?"/events/vendor/landing":"/events/organizer/landing";
+        window.location.replace(route);
     }
+
 });
